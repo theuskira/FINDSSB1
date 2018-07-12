3991416390a355ab10dab1b44a9405949591f542
theuskira <matheusviana.tk@gmail.com>
2018-07-12 17:14:06 -0300

    Initial commit

---
+++ app/src/main/java/br/com/icoddevelopers/findssp/LoginActivity.java
@@ -0,0 +1,43 @@
+package br.com.icoddevelopers.findssp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btlogin = (Button) findViewById(R.id.btLogin);
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+
+                if(login.equals("icoddevelopers") && senha.equals("icodicod")){
+                    alert("Login Realizado com Sucesso!");
+                    startActivity(new Intent(getBaseContext(), MainActivity.class));
+                    finish();
+                }else{
+                    alert("Usuario ou Senha Invalido.");
+                }
+
+            }
+        });
+    }
+
+    private void alert(String s){
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+}
---
+++ app/src/main/java/br/com/icoddevelopers/findssp/SplashActivity2.java
@@ -0,0 +1,27 @@
+package br.com.icoddevelopers.findssp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
