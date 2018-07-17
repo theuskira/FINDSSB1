26f2693c01950b0a91ea2f2389ec1b32b71af924
theuskira <matheusviana.tk@gmail.com>
2018-07-16 21:30:04 -0300

    Correcao dos bugs de menu item

--- app/src/main/java/br/com/icoddevelopers/findssp/views/MainActivity.java
+++ app/src/main/java/br/com/icoddevelopers/findssp/views/MainActivity.java
@@ -5,22 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.icoddevelopers.findssp.R;
+import br.com.icoddevelopers.findssp.cadasto.CadastroActivity;
+import br.com.icoddevelopers.findssp.cadasto.CadastroEmpresaActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
     }
 
     public void enviarSupermercado(View view){
@@ -43,5 +42,23 @@ public class MainActivity extends AppCompatActivity {
         inflar.inflate(R.menu.menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.botaoContaMenu){
+            Intent intentUsuario = new Intent(this, CadastroActivity.class);
+            startActivity(intentUsuario);
+        }else if(id == R.id.botaoContaEmpresaMenu){
+            Intent intentEmpresa = new Intent(this, CadastroEmpresaActivity.class);
+            startActivity(intentEmpresa);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
     }
 }
