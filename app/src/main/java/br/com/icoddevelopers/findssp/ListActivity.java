2f6ad4ef8e268dd83b6b8a66ccd3c711e1417178
theuskira <matheusviana.tk@gmail.com>
2018-07-31 12:29:26 -0300

    List v1

---
+++ app/src/main/java/br/com/icoddevelopers/findssp/ListActivity.java
@@ -0,0 +1,101 @@
+package br.com.icoddevelopers.findssp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.com.icoddevelopers.findssp.views.CadastroProdutosActivity;
+
+public class ListActivity extends AppCompatActivity implements View.OnClickListener{
+    BancoDado db;
+    ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        this.mViewHolder.nomeSupermercado = (TextView) findViewById(R.id.nomeSupermercado);
+        this.mViewHolder.produtoList = (TextView) findViewById(R.id.produtoList);
+        this.mViewHolder.produtoCorredor = (TextView) findViewById(R.id.produtoCorredor);
+        this.mViewHolder.produtoPrateleira = (TextView) findViewById(R.id.produtoPrateleira);
+        this.mViewHolder.produtoPesquisa = (EditText) findViewById(R.id.produtoPesquisa);
+        this.mViewHolder.addProdutoList = (Button) findViewById(R.id.btAddProduto);
+        this.mViewHolder.btPesquisarProduto = (Button) findViewById(R.id.btPesquisarProduto);
+
+
+        Intent intent = getIntent();
+        final String supermercado = intent.getStringExtra("Supermercado");
+
+        try {
+            db.list_Supermercado(supermercado, mViewHolder.nomeSupermercado);
+            db.close();
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        mViewHolder.btPesquisarProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    db.list_Products(supermercado, mViewHolder.produtoPesquisa.getText().toString(), mViewHolder.produtoList, mViewHolder.produtoCorredor, mViewHolder.produtoPrateleira);
+                    db.close();
+                }catch (Exception e){
+                    Toast.makeText(ListActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        mViewHolder.addProdutoList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ListActivity.this, "Funçao Não Implementada!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+    private class ViewHolder{
+        TextView nomeSupermercado;
+        TextView produtoList;
+        TextView produtoCorredor;
+        TextView produtoPrateleira;
+        EditText produtoPesquisa;
+        Button addProdutoList;
+        Button btPesquisarProduto;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflar = getMenuInflater();
+        inflar.inflate(R.menu.salvarlista, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.concluirLista){
+            Intent intentUsuario = new Intent(this, ListFullActivity.class);
+            startActivity(intentUsuario);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
